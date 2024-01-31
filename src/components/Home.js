3cb86ed29417dab64be58a1894291e6e203bf5b0
Vimaser <thfeske@gmail.com>
2024-01-31 15:19:34 -0600

    css and mp4

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./css/Home.css";
 /* import Logout from "./Logout"; */
 import "./css/darkMode.css";
 import tiger from "../img/tiger.png";
+import backgroundVideo from "../img/TIGERSOFTWARE.mp4";
 
 function Home() {
   const isDarkMode = document.body.getAttribute("data-dark") === "true";
+  const [showVideo, setShowVideo] = useState(true);
+
+  const handleVideoEnd = () => {
+    setShowVideo(false);
+  };
+
+  const skipVideo = () => {
+    setShowVideo(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", skipVideo);
+    window.addEventListener("keydown", skipVideo);
+
+    return () => {
+      window.removeEventListener("click", skipVideo);
+      window.removeEventListener("keydown", skipVideo);
+    };
+  }, []);
 
   return (
     <div className={`home-container ${isDarkMode ? "dark-mode" : ""}`}>
+      {/* Video Popup */}
+      {showVideo && (
+        <div className="video-popup">
+          <video
+            autoPlay
+            loop={false}
+            muted
+            className={`popup-video ${!showVideo && "fade-out"}`}
+            onEnded={handleVideoEnd}
+          >
+            <source src={backgroundVideo} type="video/mp4" />
+          </video>
+        </div>
+      )}
+
+      {/* Okay, honestly took these out because I didn't like how they looked and I'm keeping them commented in as reference. It's my site lol, I do what I want *in the voice of Eric Cartman* */}
+      {/* Video Background */}
+      {/*       <video autoPlay loop muted className="backgroud-video">
+        <source src={backgroundVideo} type="video/mp4" />
+      </video> */}
       {/* Hero Section */}
       <section className="hero-section">
         <img src={tiger} alt="Tiger" className="tiger-image" />
@@ -21,7 +61,7 @@ function Home() {
       {/* About Section */}
       <section className="about-section">
         <h2>About Us</h2>
-        <br/>
+        <br />
         <p>
           We are a team of dedicated web developers with expertise in modern web
           technologies. Trei Feske, the visionary behind Tiger Software
@@ -32,7 +72,7 @@ function Home() {
           Trei established Tiger Software Developers to revolutionize the web
           development landscape where it's often overlooked.
         </p>
-        <br/>
+        <br />
         <p>
           Trei's team is a testament to his journey, comprised of fellow cohort
           members from both college and bootcamp. Although spread out across the
@@ -40,7 +80,7 @@ function Home() {
           enabling seamless collaboration. This unity allows them to
           consistently exceed client expectations.
         </p>
-        <br/>
+        <br />
         <p>
           At the core of Trei's ethos is transparency and open communication. He
           firmly believes in being there for his customers, quite literally at
@@ -48,6 +88,18 @@ function Home() {
           availability. With Trei and Tiger Software Developers, clients don't
           just get a service; they gain a committed partner in their digital
           journey.
+        </p>
+        <br />
+        <p>
+          In addition to our commitment to quality and customer service, we
+          pride ourselves on being affordable and competitive in our pricing.
+          Unlike template-based solutions offered by platforms like Squarespace,
+          WordPress, and GoDaddy, our team of real developers writes actual
+          code, utilizing the latest frameworks and technologies. This approach
+          allows us to create bespoke, high-performance web solutions that are
+          tailored specifically to your needs, all while remaining
+          cost-effective. Choose Tiger Software Developers for cutting-edge web
+          development that aligns with your budget.
         </p>
       </section>
 
@@ -67,31 +119,77 @@ function Home() {
         {/* ... Add more services as needed */}
       </section>
 
-      {/* Testimonials (Optional) */}
+      {/* Testimonials */}
       <section className="testimonials-section">
         <h2>What Our Clients Say</h2>
         <blockquote>
-          <p>
-            "Tiger Software Developers transformed our online presence! Highly
-            recommend them!"
-          </p>
-          <cite>- FRC</cite>
+          {/* First Testimonial */}{" "}
+          <a
+            href="http://danbroemusic.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>"...I love, love, love the Website."</p>
+            <cite>- Dan Broe</cite>
+          </a>
           <br />
           <br />
-          <p>
-            "I now have a website that you can go check out. Just click the link
-            above to visit the website!! And if you’re looking to start your own
-            website, contact Tiger Software Developers!!"
-          </p>
-          <cite>- Aaron Justice</cite>
+          {/* Second Testimonial */}
+          <a
+            href="http://aaron-justice-music.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>
+              "I now have a website that you can go check out. Just click the
+              link above to visit the website!! And if you’re looking to start
+              your own website, contact Tiger Software Developers!!"
+            </p>
+            <cite>- Aaron Justice</cite>
+          </a>
+          <br />
+          <br />
+          {/* Third Testimonial */}
+          <a
+            href="http://frc-louisiana.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>
+              "Tiger Software Developers transformed our online presence! Highly
+              recommend them!"
+            </p>
+            <cite>- FRC</cite>
+          </a>
+          <br />
+          <br />
+          {/* Fourth Testimonial */}
+          <a
+            href="https://thefleamarketoflouisiana.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>"They went above and beyond our expectations!"</p>
+            <cite>- The Flea Market of Louisiana</cite>
+          </a>
+          <br />
+          <br />
+          {/* Fifth Testimonial */}
+          <a
+            href="https://unclechess.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>"You guys are awesome!"</p>
+            <cite>- Uncle Chess</cite>
+          </a>
         </blockquote>
-        {/* ... Add more testimonials as needed */}
       </section>
 
       {/* Call to Action */}
       <section className="cta-section">
         <h2>Ready to start your project?</h2>
-        <br/>
+        <br />
         <Link to="/contact" className="btn btn-success">
           Get in Touch
         </Link>
