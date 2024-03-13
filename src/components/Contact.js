21c35e8c160772126bce89f0bbce237894100d70
Vimaser <thfeske@gmail.com>
2024-03-12 22:55:08 -0500

    add business email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -33,6 +33,11 @@ const Contact = () => {
   return (
     <div className="contact-container">
       <h2>Contact Us</h2>
+      <h2>
+        <a href="mailto:tigersoftwaredevelopers@gmail.com">
+          tigersoftwaredevelopers@gmail.com
+        </a>
+      </h2>
       {submitted ? (
         <p>Thanks for reaching out! We'll get back to you soon.</p>
       ) : (
@@ -80,6 +85,7 @@ const Contact = () => {
           Contact us directly at: <span>1-225-678-1294</span>
         </p>
       </div>
+      <br/>
     </div>
   );
 };
